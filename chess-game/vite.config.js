9ecb94767be718f47c341a52baa1999e2479170c
Thomas Klok Rohde <thomas.rohde@danskebank.dk>
2024-07-04 10:09:56 +0200

    Fixed icons

--- chess-game/vite.config.js
+++ chess-game/vite.config.js
@@ -1,11 +1,26 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', 
+  base: '/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-  }
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.').at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+      },
+    },
+  },
+  publicDir: 'public',
 })
